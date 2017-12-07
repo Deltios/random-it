82a518102307fe06c3862c2928c5c8b50d73ad24
KarelAlofs <r0624198@student.thomasmore.be>
2017-12-07 11:32:10 +0100

    Auto stash before merge of "master" and "origin/master"
    Categorie filter op product pagina

--- src/main/java/com/realdolmen/thomasmore/service/ProductService.java
+++ src/main/java/com/realdolmen/thomasmore/service/ProductService.java
@@ -12,18 +12,29 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public void createProduct(String naam, String omschrijving, double prijs, int hoeveelheidInVoorraad) {
+    public void createProduct(String naam, String omschrijving, double prijs, int hoeveelheidInVoorraad, int merkId, int categorieId) {
         Product product = new Product();
         product.setNaam(naam);
         product.setOmschrijving(omschrijving);
         product.setPrijs(prijs);
         product.setHoeveelheidInVoorraad(hoeveelheidInVoorraad);
+        product.setMerkId(merkId);
+        product.setCategorieId(categorieId);
 
         productRepository.save(product);
     }
 
     public List<Product> findAllProducts() {
         return productRepository.findAll();
+    }
+
+    public List<Product> findAllProductsByCategory(int id){
+        if (id == 0){
+            return productRepository.findAll();
+        }
+        else{
+            return productRepository.findAllByCategorieId(id);
+        }
     }
 
     public Product getProduct(Long id) {
